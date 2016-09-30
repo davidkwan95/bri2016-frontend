70a691e030391d90184bc3307784c2ec5aa99f7c
David <davidkwan95@gmail.com>
2016-09-30 16:52:24 +0700

    further setup

---
+++ src/Transaksi/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Transaksi extends Component {
+  render() {
+    return (
+    	<div>
+    		Transaksi
+    	</div>
+    )
+  }
+}
--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react'
 import { Router, Route, browserHistory } from 'react-router'
+import Transaksi from './Transaksi'
 
 export default class App extends Component {
   render() {
     return (
       <Router history={browserHistory}>
         <Route path='/' component={Home} />
-        <Route path='/address' component={Address} />
+        <Route path='/transaksi' component={Transaksi} />
       </Router>
     )
   }
 }
 
 const Home = () => <h1>Hello from Home!</h1>
-const Address = () => <h1>We are located at 555 Jackson St.</h1>
